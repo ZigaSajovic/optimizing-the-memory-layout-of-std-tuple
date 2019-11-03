4c75451d80b60c375ad7f4736b08632ab27bde7e
ZigaSajovic <ziga.sajovic@gmail.com>
2019-11-03 02:01:31 +0100

    Fixed main

--- main.cpp
+++ main.cpp
@@ -1,18 +1,28 @@
 #include "Tuple.hpp"
-#include <iostream>
 #include <assert.h>
+#include <iostream>
 
 int main() {
 
-  Tuple<char, int, char, int, char, double, char> tup0{'a', 1,   'c', 3,
-                                                       'd', 5.0, 'e'};
+  Tuple<char, int, char, int, char, double, char> tup{'a', 1,   'c', 3,
+                                                      'd', 5.0, 'e'};
+  std::cout << "Size of out Tuple: " << sizeof(tup) << " Bytes" << std::endl;
 
-  std::cout << sizeof(tup0) << " Bytes" << std::endl;
+  std::tuple<char, int, char, int, char, double, char> std_tup{'a', 1,   'c', 3,
+                                                               'd', 5.0, 'e'};
+  std::cout << "Size of out std::tuple: " << sizeof(std_tup) << " Bytes"
+            << std::endl;
 
-  std::tuple<char, int, char, int, char, double, char> tup1{'a', 1,   'c', 3,
-                                                            'd', 5.0, 'e'};
-  std::cout << sizeof(tup1) << " Bytes" << std::endl;
+  std::cout << "Actual size of data: "
+            << 4 * sizeof(char) + 2 * sizeof(int) + sizeof(double) << " Bytes"
+            << std::endl;
 
-  std::cout << get<2>(tup0) << " == " << std::get<2>(tup1) << std::endl;
-  assert(tup0 == tup1);
+  std::cout << get<2>(tup) << " == " << std::get<2>(std_tup) << std::endl;
+
+  std::tuple<char, int, char> t0;
+  std::tuple<int> t1;
+  std::cout << "Size of t0: " << sizeof(t0) << std::endl;
+  std::cout << "Size of t1: " << sizeof(t1) << std::endl;
+
+  assert(tup == std_tup);
 }
