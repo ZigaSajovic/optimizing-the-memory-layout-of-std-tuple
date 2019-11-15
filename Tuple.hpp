a3f4b6c87fd560a6dc32c53a95c5c692438091cb
ZigaSajovic <ziga.sajovic@gmail.com>
2019-11-15 18:12:13 +0100

    Updated CppML and code (still need to update README)

--- Tuple.hpp
+++ Tuple.hpp
@@ -1,57 +1,50 @@
 #include "CppML/CppML.hpp"
 #include <tuple>
-using Predicate =
-    ml::Map<ml::UnList<ml::Get<1, ml::AligmentOf<>>>, ml::Greater<>>;
 
-template <typename Id, typename T> struct Tag {};
-template <typename... Ts>
-using TaggedPermutation =
-    ml::Invoke<ml::ZipWith<Tag, ml::Sort<Predicate>>,
-               typename ml::TypeRange<>::template f<0, sizeof...(Ts)>,
-               ml::ListT<Ts...>>;
-
-template <int N, typename List, typename Pipe = ml::ToList>
-using Extract =
-    ml::Invoke<ml::UnList<ml::Map<ml::UnList<ml::Get<N>>, Pipe>>, List>;
-
-template <typename... Ts> class Tuple {
-  using _TaggedPermutation = TaggedPermutation<Ts...>;
-  using Permutation = Extract<0, _TaggedPermutation>;
-  using _Tuple = Extract<1, _TaggedPermutation, ml::F<std::tuple>>;
-  template <typename T>
-  using Finder =
-      ml::Invoke<ml::UnList<ml::FindIf<ml::Partial<ml::IsSame<>, T>>>,
-                 Permutation>;
-  using InversePermutation =
-      typename ml::TypeRange<ml::Map<ml::WrapIn1<Finder>>>::template f<
-          0, ml::Length<Permutation>::value>;
-  template <int I>
-  using Index = ml::Invoke<ml::UnList<ml::Get<I>>, InversePermutation>;
-
-  _Tuple _tuple;
-  template <int... Is, typename... Us>
-  Tuple(ml::ListT<ml::Int<Is>...>, std::tuple<Us...> &&fwd)
-      : _tuple{static_cast<ml::Invoke<ml::Get<Is>, Us...> &&>(
-            std::get<Is>(fwd))...} {}
+template <typename Permutation, typename Tuple> struct TupleBase;
+template <int... Is, typename... Ts>
+struct TupleBase<ml::ListT<ml::Int<Is>...>, std::tuple<Ts...>> {
+private:
+  std::tuple<Ts...> _tuple;
+  template <typename... Us>
+  TupleBase(ml::_, std::tuple<Us...> &&fwd)
+      : _tuple{
+            static_cast<ml::f<ml::Get<Is>, Us...> &&>(std::get<Is>(fwd))...} {}
 
 public:
   template <typename... Us>
-  Tuple(Us &&... us)
-      : Tuple{Permutation{}, std::forward_as_tuple(static_cast<Us &&>(us)...)} {
-  }
-  template <int I, typename... Us> friend decltype(auto) get(Tuple<Us...> &tup);
+  TupleBase(Us &&... us)
+      : TupleBase{ml::_{}, std::forward_as_tuple(static_cast<Us &&>(us)...)} {}
+  template <typename I> // Compute the inverse index
+  using f = ml::f<ml::FindIf<ml::Partial<ml::IsSame<>, I>>, ml::Int<Is>...>;
   template <int I, typename... Us>
-  friend decltype(auto) get(const Tuple<Us...> &tup);
+  friend decltype(auto) get(TupleBase<Us...> &tup);
+  template <int I, typename... Us>
+  friend decltype(auto) get(const TupleBase<Us...> &tup);
 };
 
-template <int I, typename... Ts> decltype(auto) get(const Tuple<Ts...> &tup) {
-  using _I = typename Tuple<Ts...>::template Index<I>;
-  return std::get<_I::value>(tup._tuple);
+template <int I, typename... Us> decltype(auto) get(TupleBase<Us...> &tup) {
+  return std::get<ml::f<TupleBase<Us...>, ml::Int<I>>::value>(tup._tuple);
 }
-template <int I, typename... Ts> decltype(auto) get(Tuple<Ts...> &tup) {
-  using _I = typename Tuple<Ts...>::template Index<I>;
-  return std::get<_I::value>(tup._tuple);
+
+template <int I, typename... Us>
+decltype(auto) get(const TupleBase<Us...> &tup) {
+  return std::get<ml::f<TupleBase<Us...>, ml::Int<I>>::value>(tup._tuple);
 }
+
+template <typename... Ts>
+using MakeBase = ml::f<
+    ml::ZipWith<
+        ml::ListT,
+        ml::Sort<ml::Map<ml::Unwrap<ml::Get<1, ml::AlignOf<>>>, ml::Greater<>>,
+                 ml::Product<ml::Map<ml::Unwrap<ml::Get<0>>>,
+                             ml::Map<ml::Unwrap<ml::Get<1>>, ml::F<std::tuple>>,
+                             ml::F<TupleBase>>>>,
+    ml::Range<>::f<0, sizeof...(Ts)>, ml::ListT<Ts...>>;
+
+template <typename... Ts> struct Tuple : MakeBase<Ts...> {
+  using MakeBase<Ts...>::MakeBase;
+};
 
 template <int... Is, template <class...> class T, typename... Ts,
           template <class...> class U, typename... Us, typename F>
@@ -63,7 +56,7 @@ decltype(auto) envoker(ml::ListT<ml::Int<Is>...>, const T<Ts...> &t,
 
 template <typename... Ts, typename... Us>
 auto operator==(const std::tuple<Ts...> &lhs, const Tuple<Us...> &rhs) -> bool {
-  using List = ml::TypeRange<>::f<0, sizeof...(Ts)>;
+  using List = ml::Range<>::f<0, sizeof...(Ts)>;
   return envoker(List{}, lhs, rhs, [](auto &&x, auto &&y) { return x == y; });
 };
 
